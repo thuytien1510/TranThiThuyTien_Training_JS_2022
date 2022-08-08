f56003ef7390b8e29bbd1807076c695373143353
thuytien1510 <thuytiencoder@gmail.com>
2022-08-08 17:17:47 +0700

    update ex1

--- ex1/assets/app.js
+++ ex1/assets/app.js
@@ -5,7 +5,6 @@ let classes = (classes) => document.getElementsByClassName(classes);
 let query = (select) => document.querySelector(select);
 
 let username = id("name"),
-    email = id("email"),
     password = id("password"),
     phone = id("phone"),
     birthday = id("birthday"),
@@ -21,14 +20,40 @@ let username = id("name"),
     dataImg = query(".avatar_preview")
 birthday.max = new Date().toISOString().split("T")[0];
 //check data
-let error = (id, serial, message) => {
-    errorMsg[serial].innerHTML = message;
+let error = (id, message) => {
+    id.parentElement.querySelector('.error').innerHTML = message;
     id.style.border = "2px solid red";
 };
-let success = (id, serial) => {
-    errorMsg[serial].innerHTML = '';
+let success = (id) => {
+    id.parentNode.querySelector('.error').innerHTML = '';
     id.style.border = "2px solid";
 }
+const notEmpty = (value) => {
+    if(value.trim() != ''){
+        return true;
+    }
+    else return false
+}
+document.addEventListener('click', e => {
+    if (notEmpty(password.value) && notEmpty(confirmPassword.value)) {
+        checkMatchPassword();
+    }
+    if (notEmpty(password.value)) {
+        checkPassword();
+    }
+    if (notEmpty(email.value)) {
+        checkEmail();
+    }
+    if (notEmpty(phone.value)) {
+        checkPhone();
+    }
+    if (notEmpty(birthday.value)) {
+        checkBirthday();
+    }
+    if (notEmpty(username.value)) {
+        checkName();
+    }
+})
 function checkValidate() {
     checkName();
     checkBirthday();
@@ -36,92 +61,42 @@ function checkValidate() {
     checkEmail();
     checkPassword();
     checkMatchPassword();
-    if (
-        checkName() && checkBirthday() && checkPhone() && checkEmail() && checkPassword() && checkMatchPassword()
-    ) {
-        dataName.innerHTML = `<div class="fullName">Full Name: ${username.value}</div>`;
-        dataBirthday.innerHTML = `<div class="birthday">Birthday: ${formatDate(birthday.value)}</div>`;
-        dataEmail.innerHTML = `<div class="email">Email: ${email.value}</div>`;
-        dataPhone.innerHTML = `<div class="phone">Phone: ${formatPhoneNumber(phone.value)}</div>`;
-        dataImg.style.display = "block";
-        query('.avatar_under').style.display = "none";
-    }
 }
-//click add
-btnAdd.addEventListener('click', (e) => {
-    e.preventDefault();
-    checkValidate();
-});
-//add by key shift
-document.addEventListener('keydown', function (e) {
-    if (e.keyCode == '16') {
-        checkValidate();
+const hasError = (id) => {
+    if(id.parentElement.querySelector('.error').textContent != ''){
+        return true;
     }
-});
-//click reset
-btnReset.addEventListener('click', (e) => {
-    e.preventDefault();
-    form.reset();
-    id('avatar_upload').style.display = 'none';
-    id('text_upload').style.display = 'block';
-    id('text_upload').style.marginLeft = '40px';
-})
-//reset by key delete 
-document.addEventListener('keydown', function (event) {
-    if (event.keyCode == '46') {
-        form.reset();
-    }
-});
-
-document.addEventListener('click', e => {
-    if (password.value != '' && confirmPassword.value != '') {
-        checkMatchPassword();
-    }
-    if (password.value != '') {
-        checkPassword();
-    }
-    if (email.value != '') {
-        checkEmail();
-    }
-    if (phone.value != '') {
-        checkPhone();
-    }
-    if (birthday.value != '') {
-        checkBirthday();
-    }
-    if (username.value != '') {
-        checkName();
-    }
-})
-
+    else return false;
+}
 //alway check
 username.addEventListener('keyup', e => {
-    if (errorMsg[0].textContent != '') {
+    if (hasError(username)) {
         checkName();
     }
 })
 email.addEventListener('keyup', e => {
-    if (errorMsg[1].textContent != '') {
+    if (hasError(email)) {
         checkEmail();
+        checkName();
     }
 })
 phone.addEventListener('keyup', e => {
-    if (errorMsg[2].textContent != '') {
+    if (hasError(phone)) {
         checkPhone();
     }
 })
 birthday.addEventListener('keyup', e => {
-    if (errorMsg[3].textContent != '') {
+    if (hasError(birthday)) {
         checkBirthday();
     }
 })
 password.addEventListener('keyup', e => {
-    if (errorMsg[4].textContent != '') {
+    if (hasError(password)) {
         checkPassword();
     }
 })
 confirmPassword.addEventListener('keyup', e => {
-    if (errorMsg[5].textContent != '') {
+    if (hasError(confirmPassword)) {
         checkMatchPassword();
     }
 })
@@ -135,19 +110,25 @@ function formatDate(theDay) {
     dayArr = theDay.split("-");
     return dayArr[2] + "/" + dayArr[1] + "/" + dayArr[0];
 }
-
+//checkLength
+const minMaxLength = (length, min, max) => {
+    if(length < min || length > max){
+        return false;
+    }
+    else return true;
+}
 //name
 function checkName() {
     let theName = username.value;
-    if (theName.trim() === '') {
-        error(username, 0, "Username cannot be blank");
+    if (!notEmpty(theName)) {
+        error(username, "Username cannot be blank");
     }
-    else if (theName != '' && theName.length < 50 && theName.length > 3) {
-        success(username, 0);
+    else if (notEmpty(theName) && minMaxLength(theName.length,3,50)) {
+        success(username);
         return true;
     }
     else {
-        error(username, 0, "Name must be more than 3 letters and less than 50 letters");
+        error(username, "Name must be more than 3 letters and less than 50 letters");
         return false;
     }
 }
@@ -156,14 +137,14 @@ function checkName() {
 function checkEmail() {
     let theEmail = email.value;
     let filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    if (theEmail.trim() === '') {
-        error(email, 1, "Email cannot be blank");
+    if (!notEmpty(theEmail)) {
+        error(email, "Email cannot be blank");
     }
-    else if (filter.test(theEmail) && theEmail.length > 3 && theEmail.length < 320) {
-        success(email, 1);
+    else if (filter.test(theEmail) && minMaxLength(theEmail.length,3,320)) {
+        success(email);
         return true;
     } else {
-        error(email, 1, "Incorrect email format");
+        error(email,"Incorrect email format");
         return false;
     }
 }
@@ -171,15 +152,15 @@ function checkEmail() {
 //phone
 function checkPhone() {
     let thePhone = phone.value;
-    if (thePhone.trim() === '') {
-        error(phone, 2, "Phone cannot be blank");
+    if (!notEmpty(thePhone)) {
+        error(phone, "Phone cannot be blank");
     }
     else if (thePhone[0] == 0 && thePhone.length == 10) {
-        success(phone, 2);
+        success(phone);
         return true;
     }
     else {
-        error(phone, 2, "Phone number must include 10 numbers starting with 0, format 090-123-4567")
+        error(phone, "Phone number must include 10 numbers starting with 0, format 090-123-4567")
         return false;
     }
 }
@@ -189,15 +170,15 @@ function checkBirthday() {
     let theDay = birthday.value;
     let currentDay = new Date().toISOString().split("T")[0];
     dayArr = theDay.split("-");
-    if (dayArr[2] === '' || dayArr[1] === '' || dayArr[0] === '') {
-        error(birthday, 3, "Date cannot be blank");
+    if (!notEmpty(dayArr[2]) || !notEmpty(dayArr[1])|| !notEmpty(dayArr[0])){
+        error(birthday, "Date cannot be blank");
     }
-    else if (currentDay < theDay && dayArr[2] !== '' && dayArr[1] !== '' && dayArr[0] !== '') {
-        error(birthday, 3, "Date of birth must be less than current date")
+    else if (currentDay < theDay && notEmpty(dayArr[2]) && notEmpty(dayArr[1])&& notEmpty(dayArr[0])) {
+        error(birthday, "Date of birth must be less than current date")
         return false;
     }
     else {
-        success(birthday, 3);
+        success(birthday);
         return true;
     }
 }
@@ -208,14 +189,14 @@ function checkPassword() {
     let patt = new RegExp("^.*(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])[a-zA-Z0-9@#$%^&+=]*$");
     let regExp = /[a-zA-Z]/;
     if (thePassword === '') {
-        error(password, 4, "Password cannot be blank");
+        error(password, "Password cannot be blank");
     }
-    else if (patt.test(thePassword) && regExp.test(thePassword[0]) && thePassword.length < 31) {
-        success(password, 4)
+    else if (patt.test(thePassword) && regExp.test(thePassword[0]) && minMaxLength(thePassword,8,31)) {
+        success(password)
         return true;
     }
     else {
-        error(password, 4, "Password must contain 8-30 characters, start with a letter, contain special characters, numbers, uppercase letters");
+        error(password, "Password must contain 8-30 characters, start with a letter, contain special characters, numbers, uppercase letters");
         return false;
     }
 }
@@ -224,28 +205,65 @@ function checkPassword() {
 function checkMatchPassword() {
     let theConfirmPassword = confirmPassword.value;
     let thePassword = password.value;
-    if (theConfirmPassword == thePassword && theConfirmPassword != '') {
-        success(confirmPassword, 5);
+    if (theConfirmPassword == thePassword && notEmpty(theConfirmPassword)) {
+        success(confirmPassword);
         return true;
     }
     if (theConfirmPassword != thePassword) {
-        error(confirmPassword, 5, "Password not match");
+        error(confirmPassword,  "Password not match");
         return false;
     }
 }
 
 let loadFile = function (event) {
-    let avatarUpload = id('avatar_upload');
+    let avatarUpload = query('.avatar_upload');
     avatarUpload.src = URL.createObjectURL(event.target.files[0]);
     dataImg.src = URL.createObjectURL(event.target.files[0]);
-    avatarUpload.style.display = 'block';
-    id('text_upload').style.display = 'none';
+    avatarUpload.classList.add('show');
+    query('.text_upload').classList.remove('show');
+    query('.text_upload').classList.add('hidden');
     avatarUpload.onload = function () {
         URL.revokeObjectURL(avatarUpload.src)
         URL.revokeObjectURL(dataImg.src) // free memory
     }
 };
 
-query(".input-btn").addEventListener('click', function () {
-    query(".input-file").click();
+query(".image").addEventListener('click', function () {
+    id("file-input").click();
 }, false);
+
+
+//click add
+btnAdd.addEventListener('click', (e) => {
+    e.preventDefault();
+    checkValidate();
+    dataName.innerHTML += username.value;
+    dataBirthday.innerHTML += formatDate(birthday.value);
+    dataEmail.innerHTML += email.value;
+    dataPhone.innerHTML += formatPhoneNumber(phone.value);
+    dataImg.classList.add('show');
+    query('.text_avatar_under').classList.add('hidden');
+});
+//add by key shift
+document.addEventListener('keydown', function (e) {
+    if (e.keyCode == '16') {
+        checkValidate();
+    }
+});
+//click reset
+btnReset.addEventListener('click', (e) => {
+    e.preventDefault();
+    form.reset();
+    query('.avatar_upload').classList.remove('show');
+    query('.avatar_upload').classList.add('hidden');
+    query('.text_upload').classList.remove('hidden');
+    query('.text_upload').classList.add('show');
+    query('.text_upload').style.marginLeft = '40px';
+})
+//reset by key delete 
+document.addEventListener('keydown', function (event) {
+    if (event.keyCode == '46') {
+        form.reset();
+    }
+});
+
