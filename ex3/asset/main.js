df3d86a1b57217603a9f99370ce9143994f7d9c9
thuytien1510 <thuytiencoder@gmail.com>
2022-08-10 10:13:07 +0700

    update ex3

--- ex3/asset/main.js
+++ ex3/asset/main.js
@@ -10,10 +10,6 @@ let chatA = id('chat-left__content'),
     sendB = id('btn-send-right'),
     resetA = classes('btn-reset')[0],
     resetB = classes('btn-reset')[1],
-    fromB = classes('content-left')[0],
-    fromA = classes('content-left')[1],
-    toB = classes('content-left')[0],
-    toA = classes('content-left')[1],
     boldA = classes('btn-bold')[0],
     italicA = classes('btn-italic')[0],
     boldB = classes('btn-bold')[1],
@@ -26,58 +22,67 @@ resetB.addEventListener('click', e => {
     chatB.innerHTML = '';
 })
 boldA.addEventListener('click', e => {
+    boldA.classList.toggle('active');
     messageA.classList.toggle('bold');
 })
 boldB.addEventListener('click', e => {
+    boldB.classList.toggle('active');
     messageB.classList.toggle('bold');
 })
-
 italicA.addEventListener('click', e => {
+    italicA.classList.toggle('active');
     messageA.classList.toggle('italic');
 })
 italicB.addEventListener('click', e => {
+    italicB.classList.toggle('active');
     messageB.classList.toggle('italic');
 })
-sendA.addEventListener('click', e => {
-    if(messageA.value != '' ){
-        if(messageA.className == 'message bold'){
-            chatA.innerHTML += `<p class="fromM">${messageA.value.bold()}</p>`
-            chatB.innerHTML += `<div class= "chatY"><img src="./asset/img/a.png" alt=""><p class="fromY">${messageA.value.bold()}</p></div>`
-        }
-        else if(messageA.className == 'message italic'){
-            chatA.innerHTML += `<p class="fromM">${messageA.value.italics()}</p>`
-            chatB.innerHTML += `<div class= "chatY"><img src="./asset/img/a.png" alt=""><p class="fromY">${messageA.value.italics()}</p></div>`
-        }
-        else if(messageA.className == 'message italic bold' || messageA.className == 'message bold italic'){
-            chatA.innerHTML += `<p class="fromM">${messageA.value.italics().bold()}</p>`
-            chatB.innerHTML += `<div class= "chatY"><img src="./asset/img/a.png" alt=""><p class="fromY">${messageA.value.italics().bold()}</p></div>`
-        }
-        else {
-            chatA.innerHTML += `<p class="fromM">${messageA.value}</p>`
-            chatB.innerHTML += `<div class= "chatY"><img src="./asset/img/a.png" alt=""><p class="fromY">${messageA.value}</p></div>`
-        }
+
+const styleMessM = (mess, chatM) => {
+    if (mess.value != '') {
+        let pM = document.createElement('p');
+        let message1 = document.createTextNode(`${mess.value}`);
+        chatM.append(pM);
+        pM.append(message1);
+        pM.classList.add('fromM');
+        let class1 = mess.classList.value.split(' ')[1];
+        let class2 = mess.classList.value.split(' ')[2];
+        pM.classList.add(class1);
+        pM.classList.add(class2);
     }
+}
+const styleMessY = (mess, chatY, image) => {
+    if (mess.value != '') {
+        let pY = document.createElement('p');
+        let div = document.createElement('div');
+        let img = document.createElement('img');
+        let message2 = document.createTextNode(`${mess.value}`);
+        chatY.append(div);
+        pY.append(message2);
+        pY.classList.add('fromY');
+        div.append(img);
+        div.append(pY);
+        img.src = image;
+        div.classList.add('chatY');
+        let class1 = mess.classList.value.split(' ')[1];
+        let class2 = mess.classList.value.split(' ')[2];
+        pY.classList.add(class1);
+        pY.classList.add(class2);
+    }
+}
+sendA.addEventListener('click', e => {
+    styleMessM(messageA, chatA);
+    styleMessY(messageA, chatB, "./asset/img/a.png");
     messageA.value = '';
 })
 sendB.addEventListener('click', e => {
-    if(messageB.value != ''){
-        if(messageB.className == 'message bold'){
-            chatB.innerHTML += `<p class="fromM">${messageB.value.bold()}</p>`
-            chatA.innerHTML += `<div class= "chatY"><img src="./asset/img/letter-b.png" alt=""><p class="fromY">${messageB.value.bold()}</p></div>`
-        }
-        else if(messageB.className == 'message italic'){
-            chatB.innerHTML += `<p class="fromM">${messageB.value.italics()}</p>`
-            chatA.innerHTML += `<div class= "chatY"><img src="./asset/img/letter-b.png" alt=""><p class="fromY">${messageB.value.italics()}</p></div>`
-        }
-        else if(messageB.className == 'message italic bold' || messageB.className == 'message bold italic'){
-            chatB.innerHTML += `<p class="fromM">${messageB.value.italics().bold()}</p>`
-            chatA.innerHTML += `<div class= "chatY"><img src="./asset/img/letter-b.png" alt=""><p class="fromY">${messageB.value.italics().bold()}</p></div>`
-        }
-        else {
-            chatB.innerHTML += `<p class="fromM">${messageB.value}</p>`
-            chatA.innerHTML += `<div class= "chatY"><img src="./asset/img/letter-b.png" alt=""><p class="fromY">${messageB.value}</p></div>`
-        }
-    }
+    styleMessM(messageB, chatB);
+    styleMessY(messageB, chatA, "./asset/img/letter-b.png");
     messageB.value = '';
 })
-
+document.addEventListener('keydown', e => {
+    if (e.keyCode == '13') {
+        sendA.click();
+        sendB.click();
+    }
+})
